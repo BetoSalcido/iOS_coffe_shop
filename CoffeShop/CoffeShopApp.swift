99090df6acd1119fd490f94bf7674ad4360f7bba
Beto Salcido <betho.salcidoo@gmail.com>
2025-10-02 16:08:52 -0700

    Add Project

---
+++ CoffeShop/CoffeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopApp.swift
+//  CoffeShop
+//
+//  Created by Beto Salcido on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
