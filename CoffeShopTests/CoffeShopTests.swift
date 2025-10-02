dc1f4c2b19694b24b605fa4d5a690a6e11e29c3e
Beto Salcido <betho.salcidoo@gmail.com>
2025-10-02 15:59:46 -0700

    Add Project

---
+++ CoffeShopTests/CoffeShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeShopTests.swift
+//  CoffeShopTests
+//
+//  Created by Beto Salcido on 02/10/25.
+//
+
+import Testing
+@testable import CoffeShop
+
+struct CoffeShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
